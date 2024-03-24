d5098b93002d345d347e9244142f6ef8510f42bb
Liam <2895711m@student.gla.ac.uk>
2024-03-24 17:53:01 +0000

    feat(project): add nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserModel } from 'src/models/postModel';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,20 @@ export class AppComponent implements OnInit{
   title = 'tunedIn';
 
   public isSignupOrSignInScreen = false;
+  public user: UserModel | undefined;
 
   constructor(public appService: AppService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
+	this.appService.initialising();
 	this.router.events.subscribe(() => {
 		this.isSignupOrSignInScreen = this.router.url === '/sign-in' || this.router.url === '/sign-up';
+	});
+	this.appService.returnCurrentUser().subscribe((value: any) => {
+		const userId = value.id;
+		this.appService.findProfile(userId).subscribe((value: any) => {
+			this.user = value;
+		})
 	});
   }
 
